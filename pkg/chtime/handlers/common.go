e1caf7381d4464df70969071a2c3a06b0c92eb4c
Sergey Popov <wailorman@gmail.com>
2020-05-29 21:47:26 +0300

    style: remove comments

--- pkg/chtime/handlers/common.go
+++ pkg/chtime/handlers/common.go
@@ -4,9 +4,3 @@ import "errors"
 
 // ErrNoTimeMatches _
 var ErrNoTimeMatches = errors.New("No time matches")
-
-// // Filer _
-// type Filer interface {
-// 	FullPath() string
-// 	Name() string
-// }
