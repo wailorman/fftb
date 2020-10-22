5a5bd5cc49d2f8df4efa47f1d2887ceb2e177853
Sergey Popov <wailorman@gmail.com>
2020-10-22 20:51:00 +0300

    refactor(convert): setting keyframe interval
    Remove !=0 check, it will be in goffmpeg

--- pkg/media/convert/hevc_codec.go
+++ pkg/media/convert/hevc_codec.go
@@ -35,9 +35,7 @@ func (c *HevcCodec) configure(mediaFile *ffmpegModels.Mediafile) error {
 		mediaFile.SetVideoBitRate(c.task.VideoBitRate)
 	}
 
-	if c.task.KeyframeInterval > 0 {
-		mediaFile.SetKeyframeInterval(c.task.KeyframeInterval)
-	}
+	mediaFile.SetKeyframeInterval(c.task.KeyframeInterval)
 
 	hwaccel := chooseHwAccel(c.task, c.metadata)
 
